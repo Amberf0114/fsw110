8c4e1178ad4148c2240ee46744e77c5301f5d000
Amberf0114 <amber.freeman@bryanuniversity.edu>
2021-01-30 13:41:35 -0500

    Week5

--- Assignments/Week5/FormsMEV/FormsOTH/FormsOTH.js
+++ Assignments/Week5/FormsMEV/FormsOTH/FormsOTH.js
@@ -13,25 +13,7 @@ var dob = form.age;
 var gender = form.gender;
 var place = document.getElementsByClassName("location");
 var diet = document.getElementsByClassName("diet");
-/*var checkMale = form.getElementById("mGender");
-var checkFemale = form.getElementById("fGender");
-var noneCheck = form.getElementById("noGender");
-var checkFlorida = form.getElementById("florida");
-var checkHawii = form.getElementById("hawii");
-var checkColorado = form.getElementById("colorado");
-var noRestrct = form.getElementById("none");
-var restrctVeget = form.getElementById("veget");
-var restrctVega = form.getElementById("vegan");
 
-*/
-
-//--------Tutor: Not understanding how to combine all functions needed
-//--------Need to: Check all values are present(radios/checks require?)
-//--------Need to: Display all info vertically in alert
-
-
-//checkbox function; How to do radio and others comined? 
-// Do I use multiple functions? Should for loop be rearranged?
 function afterSubmit(){
 
      for (let i = 0; i < place.length; i++) {
